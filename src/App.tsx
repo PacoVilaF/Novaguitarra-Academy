ddb4e1dd92665366cf236024d7f36ab9a129bc8f
PacoVilaF <guitarrapaco@hotmail.com>
2026-02-12 00:09:12 +0100

    Create App.tsx with main React component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Nova Guitarra Academy</h1>
+        </div>
+    );
+};
+
+export default App;
